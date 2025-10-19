8c09f90d1318c8366f2f58941191bf0ccb717541
Michael Gerzabek <michael.gerzabek@gmail.com>
2025-10-19 17:26:25 +0200

    AfterSave Hook für Leads (1)
    Legt automatisch einen Follow-Up Task an

---
+++ custom/Espo/Custom/Hooks/Lead/CreateTask.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Espo\Custom\Hooks\Lead;
+
+use Espo\Core\Hook\Hook\AfterSave;
+use Espo\ORM\Entity;
+use Espo\ORM\EntityManager;
+use Espo\ORM\Repository\Option\SaveOptions;
+
+class CreateTask implements AfterSave
+{
+    public function __construct(private EntityManager $entityManager) {}
+
+    public function afterSave(Entity $entity, SaveOptions $options): void
+    {
+        if ($entity->isNew()) {
+            $task = $this->entityManager->getNewEntity('Task');
+            $task->set([
+                'name' => 'Follow-up: Contact ' . $entity->get('name'),
+                'assignedUserId' => $entity->get('assignedUserId'),
+                'parentType' => 'Lead',
+                'parentId' => $entity->get('id'),
+                'status' => 'Not Started',
+                'dateStart' => (new \DateTime())->format('Y-m-d'),
+                'dateEnd' => (new \DateTime('+7 days'))->format('Y-m-d'),
+                'description' => $entity->get('description'),
+            ]);
+        }
+    }
+}
